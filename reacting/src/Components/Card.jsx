f5e07a664cb90d7882acd8b2709057d0ba3870cd
GITA <jgitanjali592@gmail.com>
2024-03-07 11:34:16 +0530

    this code for condition rendering

--- reacting/src/Components/Card.jsx
+++ reacting/src/Components/Card.jsx
@@ -5,17 +5,20 @@ function Card() {
     {
       image: 'https://images.unsplash.com/photo-1709656541505-36bc9c102434?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name: 'Engagged',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eveniet cumque consectetur asperiores'
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eveniet cumque consectetur asperiores',
+      KnowMore:true
     },
     {
       image: 'https://plus.unsplash.com/premium_photo-1709065154533-d53fb085f2f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name: 'Couple pic ',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eveniet cumque consectetur asperiores'
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eveniet cumque consectetur asperiores',
+      NoMore:false
     },
     {
       image: 'https://images.unsplash.com/photo-1535615615570-3b839f4359be?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       name: 'Heart',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eveniet cumque consectetur asperiores'
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eveniet cumque consectetur asperiores',
+      KnowMore:true
     }
   ];
 
@@ -30,7 +33,7 @@ function Card() {
             <div className='w-full px-3 py-4'>
               <h2 className='font-semibold'>{elem.name}</h2>
               <p className='px-5 py-2'>{elem.description}</p>
-              <button className="px-4 py-1 bg-blue-600 text-xs rounded text-zinc-100">Know More</button>
+              <button className={`px-4 py-1 ${elem.KnowMore? 'bg-sky-600' : 'bg-red-200' } text-xs rounded text-zinc-100 mt-3`}>{elem.KnowMore ? "Yes" : "No"}</button>
             </div>
           </div>
         </div>
