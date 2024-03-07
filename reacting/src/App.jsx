84e12c74682f1274fcdc93a719478f65c95e350e
GITA <jgitanjali592@gmail.com>
2024-03-07 22:38:50 +0530

    this code for using filter

--- reacting/src/App.jsx
+++ reacting/src/App.jsx
@@ -51,15 +51,41 @@ function App() {
 
   // advanced level
 
-  const [val,setVal]=useState({name:"subham" , isBanned:false});
-return(
-  <div className='p-4 '>
-    <h1>name:{val.name}</h1>
-    <h1>banned:{val.isBanned.toString()}</h1>
-    <button onClick = {()=>setVal({...val, isBanned:!val.isBanned})} className={`px-3 py-1 ${ val.isBanned ? "bg-blue-500" : "bg-red-500"} rounded-full text-white`}>change</button>
+//   const [val,setVal]=useState({name:"subham" , isBanned:false});
+// return(
+//   <div className='p-4 '>
+//     <h1>name:{val.name}</h1>
+//     <h1>banned:{val.isBanned.toString()}</h1>
+//     <button onClick = {()=>setVal({...val, isBanned:!val.isBanned})} className={`px-3 py-1 ${ val.isBanned ? "bg-blue-500" : "bg-red-500"} rounded-full text-white`}>change</button>
 
+//   </div>
+// )
+
+// const [val,setval] = useState([1,2,3,4,4,5,6,7])
+//  return(
+//    <div className='p-5'>
+//      {val.map(item=><h1>{item}</h1>)}
+//      <button onClick = {()=>setVal(()=>val.filter((item,index)=> index != val.length-1))} className='px-3 py-1 bg-blue-500 rounded-full text-white'>click</button>
+
+//    </div>
+//  )
+const [val, setVal] = useState([1, 2, 3, 4, 5, 6, 7]);
+
+return (
+  <div className='p-5'>
+    {val.map((item) => (
+      <h1 key={item}>{item}</h1>
+    ))}
+
+    <button
+      onClick={() => setVal(val.filter((item, index) => index !== 2))}
+      className='px-3 py-1 bg-blue-500 rounded-full text-white'
+    >
+      click
+    </button>
   </div>
-)
+);
+
 
 }
 
