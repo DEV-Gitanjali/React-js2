46678e3daad9e3ae5fd0a5062b83049bd3c48fa7
GITA <jgitanjali592@gmail.com>
2024-03-07 19:00:53 +0530

    this code for usestate

--- reacting/src/App.jsx
+++ reacting/src/App.jsx
@@ -1,17 +1,43 @@
-import React  from "react";
-import Card from "./Components/Card";
+import React, { useState }  from "react";
+// import Card from "./Components/Card";
 
 
 
 
-function App(){
+// function App(){
 
 
-  return(
-    <>
-      <Card/>
+//   return(
+//     <>
+//       {/* <Card/> */}
        
-    </>
-  );
+//     </>
+//   );
+// }
+// export default App;
+
+
+
+function App() {
+  // const [score, setscore] = useState(100);
+
+  // const [banned, setbanned] = useState(false);
+ 
+
+   
+  // return (
+  //   <div>
+  //    <h1 className='ml-20'>{banned.toString()}</h1>
+  //     <button  onClick={()=> setbanned(!banned)}className='px-2 py-1  ml-20 rounded-full  text-xs bg-blue-500 text-white'>ban  karo</button> 
+      
+
+       
+  //   </div>
+  // )
+
+
+
+  
 }
-export default App;
+
+export default App
