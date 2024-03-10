57b988e83ac344bedb00d40b486291c4da6e6cae
GITA <jgitanjali592@gmail.com>
2024-03-10 09:58:13 +0530

    this code for all about music app

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -4,25 +4,33 @@ import Navbar from "./Components/Navbar";
 
 function App() {
   const data = [
-    { image: "https://plus.unsplash.com/premium_photo-1709311452506-80def0868c0d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Challenger", artist: "harshi", added: false },
+    { image: "https://plus.unsplash.com/premium_photo-1681335986095-5a9585e77246?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c29uZyUyMGFydGlzdHxlbnwwfHwwfHx8MA%3D%3D", name: "Challenger", artist: "harshi", added: false },
     { image: "https://images.unsplash.com/photo-1693835777292-cf103dcd2324?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "One day", artist: "bindu", added: false },
     { image: "https://images.unsplash.com/photo-1707944789575-3a4735380a94?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNvbmclMjBhcnRpc3R8ZW58MHx8MHx8fDA%3D", name: "One love", artist: "subham", added: false },
-    { image: "https://plus.unsplash.com/premium_photo-1681335986095-5a9585e77246?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c29uZyUyMGFydGlzdHxlbnwwfHwwfHx8MA%3D%3D", name: "Together", artist: "shree", added: true }
+    { image: "https://images.unsplash.com/photo-1474959783111-a0f551bdad25?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Together", artist: "shree", added: false }
   ];
 
   const [songData, setSongData] = useState(data);
+  const handleClick = (index) => {
+    setSongData((prev) => {
+      return prev.map((item, itemIndex) => {
+        if (itemIndex === index) return { ...item, added: !item.added };
+        return item;
+      });
+    });
+  };
 
   return (
     <>
-    <div className="w-full h-screen bg-zinc-300">
-      <Navbar />
-      <div className="px-20 flex gap-10 mt-10 flex-wrap">
-        {songData.map((obj) => (
-          <Card data={obj}/>
+      <div className="w-full h-screen bg-zinc-300">
+        <Navbar data={songData} />
+        <div className="px-20 flex gap-10 mt-10 flex-wrap">
+          {songData.map((obj, index) => (
+            <Card data={obj} handleClick={handleClick} index={index} key={index} />
           ))}
+        </div>
       </div>
-    </div>
-          </>
+    </>
   );
 }
 
--- app1/src/Components/Card.jsx
+++ app1/src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Card({ data}) {
+function Card({ data, handleClick, index }) {
   const { image, artist, name, added } = data;
   return (
     <div className='h-32 w-60 bg-zinc-100 p-4 rounded-md flex gap-4 pb-10 relative mt-10'>
@@ -12,7 +12,9 @@ function Card({ data}) {
         <h3 className='text-xl leading-none font-semibold'>{name}</h3>
         <h6 className='text-sm'>{artist}</h6>
       </div>
-      <button className={`px-10 py-3 whitespace-nowrap ${added=== false ? "bg-orange-600" : "bg-orange-700" } absolute -bottom-3 left-1/2 -translate-x-[50%] text-white text-xs rounded-full`}>{added=== false ? "Add To Favorits" : "Added"}</button>
+      <button onClick={() => handleClick(index)} className={`px-10 py-3 whitespace-nowrap ${added === false ? "bg-orange-600" : "bg-yellow-400"} absolute -bottom-3 left-1/2 -translate-x-[50%] text-white text-xs rounded-full`}>
+        {added === false ? "Add To Favorites" : "Added"}
+      </button>
     </div>
   );
 }
