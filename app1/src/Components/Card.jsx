8f75481a29953c73cd8fc7573efeed452d2484ec
GITA <jgitanjali592@gmail.com>
2024-03-10 08:56:49 +0530

    this code for added the value in the card

--- app1/src/Components/Card.jsx
+++ app1/src/Components/Card.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-function Card() {
-  
+function Card({ data}) {
+  const { image, artist, name, added } = data;
   return (
-    <div className=' h-32 w-60 bg-zinc-100 p-4   rounded-md flex gap-4 pb-10 relative mt-10 '>
-      <div className='w-20 bg-orange-600 rounded-md'></div>
-
-      <div className=''>
-        <h3 className='text-xl  leading-none font-semibold '>Song Name</h3>
-        <h6 className='text-sm'> Artist Name</h6>
+    <div className='h-32 w-60 bg-zinc-100 p-4 rounded-md flex gap-4 pb-10 relative mt-10'>
+      <div className='w-20 bg-orange-600 rounded-md'>
+        <img className='w-full h-full object-cover' src={image} alt="" />
       </div>
-      <button className='px-10 py-3 whitespace-nowrap bg-orange-600 absolute -bottom-3
-       left-1/2 -translate-x-[50%]  text-white text-xs rounded-full '>Add to Favorites</button>
+
+      <div>
+        <h3 className='text-xl leading-none font-semibold'>{name}</h3>
+        <h6 className='text-sm'>{artist}</h6>
+      </div>
+      <button className={`px-10 py-3 whitespace-nowrap ${added=== false ? "bg-orange-600" : "bg-orange-700" } absolute -bottom-3 left-1/2 -translate-x-[50%] text-white text-xs rounded-full`}>{added=== false ? "Add To Favorits" : "Added"}</button>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
