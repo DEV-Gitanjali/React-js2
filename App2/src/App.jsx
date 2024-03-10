7cc0b417c8a6a5bd0c490417d7585d0adb0fc026
GITA <jgitanjali592@gmail.com>
2024-03-10 17:17:56 +0530

    this code for main App container

---
+++ App2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
+      <div  className='container mx-auto'>
+        hiii
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
