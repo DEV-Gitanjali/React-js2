2f28c13680209843465d4fbd66fc5855cc2b3189
GITA <jgitanjali592@gmail.com>
2024-03-10 08:11:32 +0530

    this code for adding card image & details

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -4,12 +4,22 @@ import Card from "./Components/Card";
 import Navbar from "./Components/Navbar";
 
 function App() {
+
+  const data=[{image: "https://plus.unsplash.com/premium_photo-1709311452506-80def0868c0d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"Challenger" , artist:"harshi"  , added:false},
+  {image:"https://images.unsplash.com/photo-1693835777292-cf103dcd2324?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"" , artist:"bindu" , added:false},
+  {image: "https://images.unsplash.com/photo-1707944789575-3a4735380a94?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNvbmclMjBhcnRpc3R8ZW58MHx8MHx8fDA%3D" , name:"Challenger" , artist:"subham"  , added:false},
+  {image :"https://plus.unsplash.com/premium_photo-1681335986095-5a9585e77246?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c29uZyUyMGFydGlzdHxlbnwwfHwwfHx8MA%3D%3D" , name:"Challenger" , artist:"shree"  , added:false}, ];
+  
+  
   return (
 
     <>
     <div className="w-full h-screen bg-zinc-300">
       <Navbar />
+      <div className="px-20 flex gap-10 mt-10 flex-wrap">
       <Card />
+
+      </div>
 
     </div>
     </>
--- app1/src/Components/Card.jsx
+++ app1/src/Components/Card.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 function Card() {
+  
   return (
-    <div className='w-60 bg-zinc-100 p-4 rounded-md flex gap-4' pb>
+    <div className=' h-32 w-60 bg-zinc-100 p-4   rounded-md flex gap-4 pb-10 relative mt-10 '>
       <div className='w-20 bg-orange-600 rounded-md'></div>
 
       <div className=''>
         <h3 className='text-xl  leading-none font-semibold '>Song Name</h3>
         <h6 className='text-sm'> Artist Name</h6>
       </div>
-      <button className='px-4 py-3 bg-orange-600 absolute text-white text-xs rounded-full '></button>
+      <button className='px-10 py-3 whitespace-nowrap bg-orange-600 absolute -bottom-3
+       left-1/2 -translate-x-[50%]  text-white text-xs rounded-full '>Add to Favorites</button>
     </div>
   )
 }
