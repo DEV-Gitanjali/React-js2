5d3daea70cf0d51fe4540abe33ee66e9e0b3b1e0
GITA <jgitanjali592@gmail.com>
2024-03-10 22:04:30 +0530

    this code for cards design

--- App2/src/Components/Card.jsx
+++ App2/src/Components/Card.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='w-52 h-full bg-zinc-100 rounded-lg flex flex-col items-center p-2 '>
-        <div className='image w-[3vw] h-[3vw] rounded-full bg-blue-800 overflow-hidden'></div>
-        <h1 className='text-xl font-semibold '>Harsh</h1>
-        <p className='text-center text-xs font-semibold  leading-none tracking-tight '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis labore praesentium cum eveniet! Et sed, eius nulla facilis dignissimos, aut asperiores fuga eaque quibusdam porro consequuntur corporis, in deserunt quo!</p>
+    <div className='w-60 h-full bg-zinc-100 rounded-lg flex flex-col items-center p-2 '>
+        <div className='image w-[3vw] h-[3vw] rounded-full bg-zinc-800 overflow-hidden'>
+
+          {/* <img  className= 'w-full h-full object-cover' src="" alt="" /> */}
+        </div>
+
+        <h1 className=' mt-1 text-xl font-semibold '>Harsh</h1>
+        <p className='mt-2 text-center text-xs font-semibold  leading-none tracking-tight '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis labore praesentium cum eveniet! Et sed, eius nulla facilis dignissimos!</p>
+
+        <button className='px-3 py-1 bg-red-600 text-xs rounded-md  font-semibold text-white mt-1'>Remove It</button>
       
     </div>
   )
