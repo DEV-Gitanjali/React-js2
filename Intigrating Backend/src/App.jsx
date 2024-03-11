30da136024d19d7127935e3d1f100a5ae612af7a
GITA <jgitanjali592@gmail.com>
2024-03-11 23:16:39 +0530

    this code for get & send the data in database

--- Intigrating Backend/src/App.jsx
+++ Intigrating Backend/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
 
   const getproducts = () =>{
 
-    const api = "https://fakeapi.platzi.com/en/rest/products";
+    const api = "https://fakestoreapi.com/products";
     axios
     .get(api)
     .then((products)=>{
@@ -14,17 +14,48 @@ const App = () => {
 
     .catch((err)=>console.log(err));
   }
+
+
+
+    const addproducts = () =>{
+
+      const api = "https://fakestoreapi.com/products";
+      axios
+      .post(api , {
+        title: 'test product',
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+      })
+      .then((res)=>{
+  
+        console.log(res);
+      })
+  
+      .catch((err)=>console.log(err));
+
+  };
+
+
   return (
     <div className="pt-[5%] pl-[5%]">
 
       <button 
        onClick={getproducts}
-      className="rounded px-5 py-2 bg-red-300">
+      className="rounded px-5 py-2 bg-red-300 ">
         call product API
+      </button>
+
+<br/> <b/>
+      <button 
+       onClick={addproducts}
+      className="rounded px-5 py-2 bg-red-300 mt-6">
+        save new product API
       </button>
       
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
